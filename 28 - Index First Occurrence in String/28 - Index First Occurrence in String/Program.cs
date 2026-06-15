0539b10cbe74d8fe85eaec41b8d7417a049b8f40
Ian <ianpritt@yahoo.com>
2026-06-15 11:05:50 -0400

    Beats 100%

---
+++ 28 - Index First Occurrence in String/28 - Index First Occurrence in String/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(StrStr("hello", "ll"));
+
+int StrStr(string haystack, string needle)
+{
+    int index = -1;
+
+    for (int i = 0; i <= haystack.Length - needle.Length; i++)
+    {
+        // input strings are guaranteed to be lower case, so don't
+        // need string.Comparison as it has an impact on run time
+        if (haystack[i..(i + needle.Length)].Equals(needle))
+        {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+}
