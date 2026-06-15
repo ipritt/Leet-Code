396d364cc452d1fd8a8b6b4b60bf47d151a9d70e
Ian <ianpritt@yahoo.com>
2026-06-15 12:10:23 -0400

    Beats 100%

---
+++ LengthOfLastWord/LengthOfLastWord/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+
+int LengthOfLastWord(string s)
+{
+    int index = 0;
+    string[] split = s.Split(" ");
+
+    for (int i = split.Length - 1; i >= 0; i--)
+    {
+        if (split[i] != string.Empty)
+        {
+            return split[i].Length;
+        }
+    }
+    return index;
+}
