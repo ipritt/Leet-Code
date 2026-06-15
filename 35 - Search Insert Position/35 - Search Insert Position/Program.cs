d884992bd08e4d63c81fbe19f70bc26084ab2d8f
Ian <ianpritt@yahoo.com>
2026-06-15 11:57:33 -0400

    Beats 100%

---
+++ 35 - Search Insert Position/35 - Search Insert Position/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(SearchInsert([1], 2));
+
+int SearchInsert(int[] nums, int target)
+{
+    int result = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] < target)
+        {
+            result = i + 1;
+        }
+    }
+
+    return result;
+}
