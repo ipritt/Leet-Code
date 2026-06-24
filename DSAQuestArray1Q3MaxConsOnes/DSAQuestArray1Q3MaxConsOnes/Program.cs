b03b9ae6cdf8de80d7f12a41fccbf1a8142032b1
Ian <ianpritt@yahoo.com>
2026-06-23 20:55:38 -0400

    Beats 100%

---
+++ DSAQuestArray1Q3MaxConsOnes/DSAQuestArray1Q3MaxConsOnes/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine(FindMaxConsecutiveOnes(new int[] { 1, 1, 0, 1, 1, 1 }));
+
+int FindMaxConsecutiveOnes(int[] nums)
+{
+    int max = 0;
+    int currentCount = 0;
+
+    foreach(int num in nums)
+    {
+        currentCount = num == 1 ? currentCount + 1 : 0;
+
+        if (currentCount > max)
+        {
+            max = currentCount;
+        }
+    }
+
+    return max;
+}
