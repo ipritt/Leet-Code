b9ddf087de2630bdcd569387b5e1d187719bf843
Ian <ianpritt@yahoo.com>
2026-06-23 20:16:52 -0400

    Beats 100%

---
+++ DSAQuestArray1/DSAQuestArray1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine(GetConcatenation(new int[] { 1, 2, 1 }));
+
+
+int[] GetConcatenation(int[] nums)
+{
+    List<int> ans = [];
+
+    ans.InsertRange(0, nums);
+    ans.InsertRange(nums.Length, nums);
+
+
+    return [.. ans];
+}
