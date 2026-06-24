e66c5d3ea1bd7401d37602232cfea53d7a321aa4
Ian <ianpritt@yahoo.com>
2026-06-23 20:47:29 -0400

    Beats 79.4%

---
+++ DSAQuestArray1Q2/DSAQuestArray1Q2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(Shuffle(new int[] { 2, 5, 1, 3, 4, 7 }, 3));
+
+int[] Shuffle(int[] nums, int n)
+{
+    List<int> result = [.. nums];
+
+    for (int i = 1; i < nums.Length - 1; i += 2)
+    {
+        result.Insert(i, nums[i + (n - 1)]);
+        result.RemoveAt(i + n--);
+    }
+
+    return [.. result];
+}
