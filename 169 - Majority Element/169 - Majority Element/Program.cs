d7fa3cf91ce2ba404350cfaaca1ff6f5365f2bce
Ian <ianpritt@yahoo.com>
2026-07-24 18:14:34 -0400

    100%

---
+++ 169 - Majority Element/169 - Majority Element/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine(MajorityElement([2, 2, 1, 3, 1, 1, 4, 1, 1, 5, 1, 1, 6]));
+
+static int MajorityElement(int[] nums)
+{
+    int count = 0;
+    int candidate = 0;
+
+    for(int i = 0; i < nums.Length; i++)
+    {
+        if (count == 0)
+        {
+            candidate = nums[i];
+        }
+
+        if (nums[i] == candidate)
+            count++;
+        else
+            count--;
+    }
+
+    return candidate;
+}
