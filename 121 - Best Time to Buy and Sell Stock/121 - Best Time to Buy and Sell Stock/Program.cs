13ba6cfb26042f9e2f3ce34ef91eb9613104eae0
Ian <ianpritt@yahoo.com>
2026-07-24 19:30:47 -0400

    99.87%

---
+++ 121 - Best Time to Buy and Sell Stock/121 - Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine(MaxProfit([2, 1, 2, 1, 0, 1, 2]));
+
+
+static int MaxProfit(int[] prices)
+{
+    int buy = prices[0];
+    int sell = 0;
+    int profit = 0;
+
+    for (int i = 0; i < prices.Length; i++)
+    {
+        if (prices[i] < buy) 
+        {
+            buy = prices[i];
+            sell = prices[i];
+        }
+
+        if (prices[i] > sell)
+        {
+            sell = prices[i];
+
+            int currentProfit = sell - buy;
+
+            if (currentProfit > profit)
+            {
+                profit = currentProfit;
+            }
+        }
+    }
+
+    return profit;
+}
