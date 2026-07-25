40b265db3b5d91f356ec24c0b27939243026c932
Ian <ianpritt@yahoo.com>
2026-07-24 22:53:41 -0400

    100% runtime 22.21% memory - needs work

---
+++ 392 - Is Subsequence/392 - Is Subsequence/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(IsSubsequence("", "ahbgdc"));
+
+static bool IsSubsequence(string s, string t)
+{
+    if (s.Length == 0) return true;
+
+    int i = 0;
+    bool isSubsequence = false;
+
+    for (int j = 0; j < t.Length; j++)
+    {
+        if (s[i] == t[j])
+        {
+            isSubsequence = true;
+            i++;
+
+            if (i == s.Length) break;
+        }
+        else
+        {
+            isSubsequence = false;
+        }
+    }
+
+    if (i != s.Length) return false;
+
+    return isSubsequence;
+}
