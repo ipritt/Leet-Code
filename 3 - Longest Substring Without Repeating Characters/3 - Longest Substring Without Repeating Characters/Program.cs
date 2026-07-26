cb24ec50094a7cded4aeb6eac9cc92d4c3c17f91
Ian <ianpritt@yahoo.com>
2026-07-25 22:29:57 -0400

    93.11%

---
+++ 3 - Longest Substring Without Repeating Characters/3 - Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
+
+
+static int LengthOfLongestSubstring(string s)
+{
+    if (s.Length == 0) return 0;
+
+    int left = 0;
+    int count = 0;
+    var map = new Dictionary<char, int>();
+
+    for (int right = 0; right < s.Length; right++)
+    {
+        char c = s[right];
+
+        if (map.TryGetValue(c, out int value))
+        {
+            left = Math.Max(value + 1, left);
+        }
+
+        map[c] = right;
+        count = Math.Max(count, right - left + 1);
+    }
+
+    return count;
+}
