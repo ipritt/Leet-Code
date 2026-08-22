51547a56f641d421829b6b977df6d9b54b97438a
Ian <ianpritt@yahoo.com>
2026-08-22 00:05:22 -0400

    runtime 100% memory 97.6%

---
+++ 202 - Happy Number/202 - Happy Number/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine(IsHappy(19));
+
+static int Next(int x)
+{
+    int sum = 0;
+
+    while (x > 0)
+    {
+        int digit = x % 10;
+        sum += digit * digit;
+        x /= 10;
+    }
+
+    return sum;
+}
+
+static bool IsHappy(int n)
+{
+    if (n == 1) return true;
+
+    int slow = n;
+    int fast = n;
+
+    // Floyd's cycle-finding algorithm (two-pointer)
+    do
+    {
+        slow = Next(slow);
+        fast = Next(Next(fast));
+    }
+    while (fast != 1 && slow != fast);
+
+    return fast == 1;
+}
