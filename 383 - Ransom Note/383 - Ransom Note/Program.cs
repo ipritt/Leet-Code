4a0eac5d8ccba5dacf2ed115f4f545a5fc8100dd
Ian <ianpritt@yahoo.com>
2026-08-22 00:57:32 -0400

    98.87 and 67.49

---
+++ 383 - Ransom Note/383 - Ransom Note/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(CanConstruct("aa", "aab"));
+
+static bool CanConstruct(string ransomNote, string magazine)
+{
+    int[] counts = new int[26];
+
+    foreach (char c in magazine)
+    {
+        // c - 'a' is like subtracting 1 because of 0 based index
+        counts[c - 'a']++;
+    }
+
+    foreach (char c in ransomNote)
+    {
+        if (--counts[c - 'a'] < 0) return false;
+    }
+
+    return true;
+}
