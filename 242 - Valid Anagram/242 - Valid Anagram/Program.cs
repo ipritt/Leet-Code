f56b0b8e887a531d84f4f8b5fd67ca66de2216f4
Ian <ianpritt@yahoo.com>
2026-08-23 13:52:16 -0400

    runtime 65.14 memory 44.5 could probably handle in single loop - will try later

---
+++ 242 - Valid Anagram/242 - Valid Anagram/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(IsAnagram("aacc", "ccac"));
+
+
+static bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length) return false;
+
+    Dictionary<char, int> hashSet = [];
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (hashSet.TryGetValue(s[i], out int count))
+        {
+            hashSet[s[i]] = count + 1;
+        }
+        else
+        {
+            hashSet[s[i]] = 1;
+        }
+    }
+
+    for (int i = 0; i < t.Length; i++)
+    {
+        if (hashSet.TryGetValue(t[i], out int value))
+        {
+            if (value <= 0) return false;
+
+            hashSet[t[i]] = --value;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
