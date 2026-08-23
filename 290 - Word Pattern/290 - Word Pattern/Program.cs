527b2789b4cac0afbe9fc590ca4a371525c3a008
Ian <ianpritt@yahoo.com>
2026-08-23 13:14:09 -0400

    runtime 95.01 memory 40.38

---
+++ 290 - Word Pattern/290 - Word Pattern/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine(WordPattern("abba", "dog cat cat fish"));
+
+static bool WordPattern(string pattern, string s)
+{
+    string[] sArray = s.Split(" ");
+
+    if (sArray.Length != pattern.Length) return false;
+
+    Dictionary<char, string> hashset = [];
+
+    for (int i = 0; i < sArray.Length; i++)
+    {
+        if (hashset.TryGetValue(pattern[i], out string? value))
+        {
+            if (value != sArray[i])
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (hashset.ContainsValue(sArray[i]))
+            {
+                return false;
+            }
+
+            hashset.Add(pattern[i], sArray[i]);
+        }
+    }
+
+    return true;
+}
